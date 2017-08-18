c28070201c8ebd3ff46655987bbb8842738aa0c9
Dan Meyer <somesing9@gmail.com>
2017-08-17 22:06:53 -0400

    Trying to get master branch up to date to merge in my branch

---
+++ app.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var methodOverride = require('method-override');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var passport = require('passport');
+var session = require('express-session');
+var index = require('./routes/index');
+var users = require('./routes/users');
+var jobs = require('./routes/jobs');
+var models = require('./models');
+
+
+
+var app = express();
+
+// view engine setup
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: '.hbs' }));
+app.set("view engine", "hbs");
+
+// For Passport
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+app.use('/jobs', jobs);
+
+require('./config/passport/passport')(passport, models.User);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// //Sync Database
+// models.sequelize.sync().then(function() {
+//   console.log('Nice! Database looks fine')
+// }).catch(function(err) {
+//   console.log(err, "Something went wrong with the Database Update!")
+// });
+
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
